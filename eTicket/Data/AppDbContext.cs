b861dee1bd5621dec04bfc8626889e1ead00ea3b
azimjonabdugafforov17 <azimjonabdugafforov17@gmail.com>
2023-02-28 04:45:45 +0500

    changed the database connection

--- eTicket/Data/AppDbContext.cs
+++ eTicket/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,6 @@ namespace eTicket.Data
 {
     public class AppDbContext:DbContext
     {
+
     }
 }
